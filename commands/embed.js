8b8ca2ea85260f6352a77a82cf5b0e572e115c63
xStarlyx <hejuanlin88@gmail.com>
2023-07-04 17:15:09 +0800

    yth

---
+++ commands/embed.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+module.exports = {
+    data: new SlashCommandBuilder().setName("embed").setDescription("show an embed"),
+    async execute(client, interaction) {
+        const embed = new EmbedBuilder().setTitle("I'm an Embed").setURL('https://www.youtube.com/').setImage('https://play.google.com/store/apps/details?id=com.google.android.youtube&hl=zh_TW');
+        interaction.reply({ embeds: [embed] });
+    },
+};
+/*
+.setURL('放URL')
+.setAuthor({ name: '', iconURL: '放URL', url: '放URL' })
+.setDescription('')
+.setThumbnail('放URL')
+.addFields({ name: '', value: '', inline: true or false })
+.setImage('放URL')
+.setTimestamp()
+.setFooter({ text: '', iconURL: '放URL' });
+*/
